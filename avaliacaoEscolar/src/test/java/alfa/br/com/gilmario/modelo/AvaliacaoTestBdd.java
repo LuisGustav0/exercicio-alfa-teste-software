105e239f8615bf7e867358f5bcd003c5950b0f08
luisgustavo <luis.gustav0@live.com>
2019-05-04 10:30:38 -0300

    aula 04/05/2019

--- avaliacaoEscolar/src/test/java/alfa/br/com/gilmario/modelo/AvaliacaoTestBdd.java
+++ avaliacaoEscolar/src/test/java/alfa/br/com/gilmario/modelo/AvaliacaoTestBdd.java
@@ -4,6 +4,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -23,6 +24,7 @@ public class AvaliacaoTestBdd {
   }
 
   @Given("^que quero testar valores invalidos$")
+  @Test
   public void testaValoresInvalidos() {
     System.out.println("\nTestando Valores Inválidos!\n");
   }
