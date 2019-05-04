09a4757cf3a97104124eaae0758162a75139e987
luisgustavo <luis.gustav0@live.com>
2019-05-04 10:31:39 -0300

    aula 04/05/2019

--- avaliacaoEscolar/src/main/java/alfa/br/com/gilmario/modelo/CucumberRunner.java
+++ avaliacaoEscolar/src/main/java/alfa/br/com/gilmario/modelo/CucumberRunner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     format = {"pretty", "html:reports/test-report"},
-    glue = {"alfa\\br\\com\\gilmario"},
-    features = {"src\\test\\resources/avaliacao.feature"}
+    glue = {"alfa/br/com/gilmario"},
+    features = {"src/test/resources/avaliacao.feature"}
 )
 public class CucumberRunner {
 }
